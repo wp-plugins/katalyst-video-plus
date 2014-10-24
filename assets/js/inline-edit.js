e23767c4ead04c998a0a9eb13d504adbab482212
Keiser Media <Keiser Media@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-24 16:44:18 +0000

    More bug fixes
    git-svn-id: https://plugins.svn.wordpress.org/katalyst-video-plus/tags/2.0.2@1013495 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/inline-edit.js
@@ -0,0 +1,174 @@
+var inlineEditAccount;
+(function($) {
+inlineEditAccount = {
+
+	init : function(){
+		var t = this, qeRow = $('#inline-edit');
+
+		t.type = $('table.widefat').hasClass('accounts');
+		t.what = '#account-';
+
+		// prepare the edit rows
+		qeRow.keyup(function(e){
+			if ( e.which === 27 ) {
+				return inlineEditAccount.revert();
+			}
+		});
+
+		$('a.cancel', qeRow).click(function(){
+			return inlineEditAccount.revert();
+		});
+		$('a.save', qeRow).click(function(){
+			return inlineEditAccount.save(this);
+		});
+		$('td', qeRow).keydown(function(e){
+			if ( e.which === 13 ) {
+				return inlineEditAccount.save(this);
+			}
+		});
+
+		// add events
+		$('#the-list').on('click', 'a.editinline', function(){
+			inlineEditAccount.edit(this);
+			return false;
+		});
+
+		$('#doaction, #doaction2').click(function(e){
+			var n = $(this).attr('id').substr(2);
+			if ( 'edit' === $( 'select[name="' + n + '"]' ).val() ) {
+				e.preventDefault();
+			} else if ( $('form#posts-filter tr.inline-editor').length > 0 ) {
+				t.revert();
+			}
+		});
+	},
+
+	toggle : function(el){
+		var t = this;
+		$( t.what + t.getId( el ) ).css( 'display' ) === 'none' ? t.revert() : t.edit( el );
+	},
+	
+	edit : function(id) {
+		var t = this, fields, editRow, rowData, status, pageOpt, pageLevel, nextPage, pageLoop = true, nextLevel, cur_format, f;
+		t.revert();
+
+		if ( typeof(id) === 'object' ) {
+			id = t.getId(id);
+		}
+
+		fields = ['username', 'author', 'developer_key', 'oauth_id', 'oauth_secret'];
+
+		// add the new blank row
+		editRow = $('#inline-edit').clone(true);
+		$('td', editRow).attr('colspan', $('.widefat:first thead th:visible').length);
+
+		if ( $( t.what + id ).hasClass( 'alternate' ) ) {
+			$(editRow).addClass('alternate');
+		}
+		$(t.what+id).hide().after(editRow);
+
+		// populate the data
+		rowData = $('#inline_'+id);
+		if ( !$(':input[name="edit_account[author]"] option[value="edit_account[' + $('.author', rowData).text() + ']"]', editRow).val() ) {
+			// author no longer has edit caps, so we need to add them to the list of authors
+			$(':input[name="edit_account[author]"]', editRow).prepend('<option value="edit_account[' + $('.author', rowData).text() + ']">' + $('#' + t.type + '-' + id + ' .author').text() + '</option>');
+		}
+		if ( $( ':input[name="edit_account[author]"] option', editRow ).length === 1 ) {
+			$('label.inline-edit-author', editRow).hide();
+		}
+
+		for ( f = 0; f < fields.length; f++ ) {
+			$(':input[name="edit_account[' + fields[f] + ']"]', editRow).val( $('.'+fields[f], rowData).text() );
+		}
+
+		// hierarchical taxonomies
+		$('.category', rowData).each(function(){
+			var taxname,
+				term_ids = $(this).text();
+
+			if ( term_ids ) {
+				taxname = $(this).attr('id').replace('_'+id, '');
+				$('ul.'+taxname+'-checklist :checkbox', editRow).val(term_ids.split(','));
+			}
+		});
+
+		$(editRow).attr('id', 'edit-'+id).addClass('inline-editor').show();
+		$('.ptitle', editRow).focus();
+
+		return false;
+	},
+
+	save : function(id) {
+		var params, fields;
+
+		if ( typeof(id) === 'object' ) {
+			id = this.getId(id);
+		}
+
+		$('table.widefat .spinner').show();
+
+		params = {
+			action: 'kvp_inline_save',
+			action2: 'edit',
+			ID: id,
+		};
+
+		fields = $('#edit-'+id).find(':input').serialize();
+		params = fields + '&' + $.param(params);
+		
+		// make ajax request
+		$.post( ajaxurl, params,
+			function(r) {
+				$('table.widefat .spinner').hide();
+
+				if (r) {
+					if ( -1 !== r.indexOf( '<tr' ) ) {
+						$(inlineEditAccount.what+id).remove();
+						$('#edit-'+id).before(r).remove();
+						$(inlineEditAccount.what+id).hide().fadeIn();
+					} else {
+						r = r.replace( /<.[^<>]*?>/g, '' );
+						$('#edit-'+id+' .inline-edit-save .error').html(r).show();
+					}
+				} else {
+					$('#edit-'+id+' .inline-edit-save .error').html(inlineEditL10n.error).show();
+				}
+
+				if ( $('#post-'+id).prev().hasClass('alternate') ) {
+					$('#post-'+id).removeClass('alternate');
+				}
+			},
+		'html');
+		return false;
+	},
+
+	revert : function(){
+		var id = $('table.widefat tr.inline-editor').attr('id');
+
+		if ( id ) {
+			$('table.widefat .spinner').hide();
+
+			if ( 'bulk-edit' === id ) {
+				$('table.widefat #bulk-edit').removeClass('inline-editor').hide();
+				$('#bulk-titles').html('');
+				$('#inlineedit').append( $('#bulk-edit') );
+			} else {
+				$('#'+id).remove();
+				id = id.substr( id.lastIndexOf('-') + 1 );
+				$(this.what+id).show();
+			}
+		}
+
+		return false;
+	},
+
+	getId : function(o) {
+		var id = $(o).closest('tr').attr('id'),
+			parts = id.split('-');
+		return parts[parts.length - 1];
+	}
+};
+
+$( document ).ready( function(){ inlineEditAccount.init(); } );
+
+}(jQuery));
---
+++ public/class-public.php
@@ -0,0 +1,184 @@
+<?php if( ! defined('ABSPATH') ) { header('Status: 403 Forbidden'); header('HTTP/1.1 403 Forbidden'); exit(); }
+/**
+* The public-facing functionality of the plugin.
+*
+* @link       http://katalystvideoplus.com
+* @since      2.0.0
+* @package    Katalyst_Video_Plus
+* @subpackage Katalyst_Video_Plus/public
+* @author     Keiser Media <support@keisermedia.com>
+*/
+class Katalyst_Video_Plus_Public {
+
+	/**
+	 * The readable name of this plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 * @var      string The ID of this plugin.
+	 */
+	private $name;
+	
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 * @var      string The ID of this plugin.
+	 */
+	private $slug;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    2.0.0
+	 * @access   private
+	 * @var      string  The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    2.0.0
+	 * @var      string    $slug       The slug of this plugin.
+	 * @var      string    $version    The version of this plugin.
+	 */
+	public function __construct( $name, $slug, $version ) {
+		
+		$this->name = $name;
+		$this->slug = $slug;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the Dashboard.
+	 *
+	 * @since    2.0.0
+	 */
+	public function enqueue_styles() {
+
+		wp_enqueue_style( $this->slug, plugin_dir_url( dirname( __FILE__ ) ) . 'assets/css/kvp.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the dashboard.
+	 *
+	 * @since    2.0.0
+	 */
+	public function enqueue_scripts() {
+
+		wp_enqueue_script( $this->slug, plugin_dir_url( dirname( __FILE__ ) ) . 'assets/js/kvp.js', array( 'jquery' ), $this->version, false );
+
+	}
+	
+	/**
+	 * Audit current KVP page
+	 * 
+	 * @since 2.0.0
+	 */
+	public function audit_single() {
+		
+		if( !is_single() )
+			return;
+		
+		$post_id = get_the_ID();
+		$post_meta	= get_post_meta( $post_id, '_kvp', true );
+		
+		if( isset($post_meta['post_id']) ) {
+			
+			$kvp_youtube_import = new Katalyst_Video_Plus_Import;
+			$kvp_youtube_import->audit( $post_meta['post_id'] );
+			
+		}
+		
+	}
+	
+	/**
+	 * Filters the_content to display video embed code
+	 * 
+	 * @since 2.0.0
+	 */
+	public function the_content( $content ) {
+		
+		$settings	= get_option( 'kvp_settings' );
+		$post_meta	= get_post_meta( get_the_ID(), '_kvp', true );
+		
+		if( empty($post_meta['service']) || empty($post_meta['video_id']) || get_post_type() !== 'post' || ( !is_single() && !isset($settings['show_video_in_lists']) ) )
+			return $content;
+		
+		if( !isset($settings['show_video_in_lists']) && has_post_thumbnail() && !is_single() )
+			return the_post_thumbnail();
+		
+		$format = apply_filters( 'kvp_' . $post_meta['service'] . '_video_embed', '', $post_meta);
+		$size	= $this->get_thumbnail_size();
+		
+		$atts = array(
+			'video_id'	=> $post_meta['video_id'],
+			'username'	=> $post_meta['username'],
+			'height'	=> ( $size[0] * 9 / 16 ),
+			'width'		=> $size[0],
+		);
+		
+		$video_html = apply_filters( 'kvp_' . $post_meta['service'] . '_video_embed', '', $atts );
+		
+		if( !is_single() )
+			return $video_html;
+		
+		return $video_html . $content;
+		
+	}
+	
+	/**
+	 * Removes thumbnail from single posts
+	 * 
+	 * @since 2.0.0
+	 */
+	public function post_thumbnail_html( $html ) {
+		
+		$post_meta = get_post_meta( get_the_ID(), '_kvp', true );
+		
+		if( !empty($post_meta) && is_single() )
+			return '';
+		
+		return $html;
+		
+	}
+	
+	/**
+	 * Gets appropriate thumbnail size for videos
+	 */
+	private function get_thumbnail_size() {	
+		global $_wp_additional_image_sizes;
+     	
+     	if( !empty($this->thumbnail_size) )
+     		return $this->thumbnail_size;
+     	
+     	$sizes = array();
+ 		
+ 		foreach( get_intermediate_image_sizes() as $s ){
+ 			$sizes[ $s ] = array( 0, 0 );
+ 			
+ 			if( in_array( $s, array( 'thumbnail', 'medium', 'large' ) ) ){
+ 			
+ 				$sizes[ $s ][0] = get_option( $s . '_size_w' );
+ 				$sizes[ $s ][1] = get_option( $s . '_size_h' );
+ 			
+ 			} else {
+ 				if( isset( $_wp_additional_image_sizes ) && isset( $_wp_additional_image_sizes[ $s ] ) )
+ 					$sizes[ $s ] = array( $_wp_additional_image_sizes[ $s ]['width'], $_wp_additional_image_sizes[ $s ]['height'], );
+ 			}
+ 		}
+ 		
+ 		$set_size = apply_filters( 'post_thumbnail_size', 'post-thumbnail' );
+ 		
+ 		if( isset($sizes[$set_size]) && is_array($sizes[$set_size]) )
+ 			return $sizes[$set_size];
+ 		
+ 		return array( 640, 385 );
+ 		
+	}
+	
+}
